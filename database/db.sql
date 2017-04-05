20f1981a4a2c4513a47828ee6130fb5af49bc45b
Adil Belhaji <belhaji.dev@gmail.com>
2017-04-05 09:21:52 +0100

    added db

--- database/db.sql
+++ database/db.sql
@@ -1,46 +1,173 @@
-CREATE TABLE accounts (
-  id       INTEGER PRIMARY KEY AUTO_INCREMENT,
-  login    VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  name     VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL
-);
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: aldoanto
+-- ------------------------------------------------------
+-- Server version	5.7.16
 
-CREATE TABLE requests (
-  id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-  account_id  INTEGER,
-  description TEXT,
-  date        DATE,
-  amount      DOUBLE,
-  limit_date  DATE,
-  picture     VARCHAR(255),
-  status      SMALLINT,
-  CONSTRAINT FOREIGN KEY (account_id) REFERENCES accounts (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE donations (
-  id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-  account_id  INTEGER,
-  request_id  INTEGER,
-  amount      DOUBLE,
-  date        DATE,
-  receiver_id INTEGER,
-  CONSTRAINT FOREIGN KEY (account_id) REFERENCES accounts (id),
-  CONSTRAINT FOREIGN KEY (request_id) REFERENCES requests (id),
-  CONSTRAINT FOREIGN KEY (receiver_id) REFERENCES accounts (id)
-);
+--
+-- Table structure for table `accounts`
+--
 
-CREATE TABLE requests_news (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  request_id INTEGER,
-  account_id INTEGER,
-  date DATE
-);
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `accounts`
+--
 
-CREATE TABLE donations_news (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  donation_id INTEGER,
-  account_id INTEGER,
-  date DATE
-);
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'updated','test','test','user'),(2,'test2','test2','test2','assoc'),(3,'post test','test','test','user');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `donations`
+--
+
+DROP TABLE IF EXISTS `donations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `donations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) DEFAULT NULL,
+  `request_id` int(11) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `public` smallint(6) DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  KEY `request_id` (`request_id`),
+  KEY `receiver_id` (`receiver_id`),
+  CONSTRAINT `donations_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`),
+  CONSTRAINT `donations_ibfk_2` FOREIGN KEY (`request_id`) REFERENCES `requests` (`id`),
+  CONSTRAINT `donations_ibfk_3` FOREIGN KEY (`receiver_id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donations`
+--
+
+LOCK TABLES `donations` WRITE;
+/*!40000 ALTER TABLE `donations` DISABLE KEYS */;
+INSERT INTO `donations` VALUES (1,1,1,199.99,'2017-04-13',1,NULL),(2,1,2,19,'2017-04-20',1,NULL);
+/*!40000 ALTER TABLE `donations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `donations_news`
+--
+
+DROP TABLE IF EXISTS `donations_news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `donations_news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `donation_id` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donations_news`
+--
+
+LOCK TABLES `donations_news` WRITE;
+/*!40000 ALTER TABLE `donations_news` DISABLE KEYS */;
+INSERT INTO `donations_news` VALUES (1,1,1,'2017-04-19'),(2,2,1,'2017-04-20');
+/*!40000 ALTER TABLE `donations_news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) DEFAULT NULL,
+  `description` text,
+  `date` date DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `limit_date` date DEFAULT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `status` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (1,1,'test desc','2017-04-06',122.9999,'2017-04-20',NULL,0),(2,1,'sasasa','2017-04-11',19862,'2017-04-13',NULL,0),(3,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(4,1,'test hello','2017-04-06',122.99,'2017-04-20',NULL,1),(5,1,'test hello','2017-04-06',122.99,'2017-04-20',NULL,1),(6,1,'test hello','2017-04-06',122.99,'2017-04-20',NULL,1);
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests_news`
+--
+
+DROP TABLE IF EXISTS `requests_news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests_news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_id` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests_news`
+--
+
+LOCK TABLES `requests_news` WRITE;
+/*!40000 ALTER TABLE `requests_news` DISABLE KEYS */;
+INSERT INTO `requests_news` VALUES (1,1,1,'2017-04-04'),(2,2,1,'2017-04-06');
+/*!40000 ALTER TABLE `requests_news` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-05  9:21:21
